fd6f2db86aabcd3223f911b1f6516b12032146f8
slplayford-cloud <slplayford@gmail.com>
2025-12-05 17:38:15 -0500

    finished function

--- finBert_news.py
+++ finBert_news.py
@@ -3,58 +3,58 @@
 from transformers import pipeline
 import yfinance as yf
 from newsapi import NewsApiClient
+import pandas as pd
 
 #news api key: d99e2ff45573499aab19b385224cf018
+
+
+midcap_df = pd.read_csv("midcaps.csv")
 
 newsAPI = NewsApiClient(api_key='d99e2ff45573499aab19b385224cf018')
 pipe = pipeline("text-classification", model="ProsusAI/finbert")
 
-ticker = yf.Ticker("AAPL")
-keyword = "apple"
-news = ticker.news
-
 total_score = 0
 total_articles = 0
 
-#run all pulled articles through finBERT analysis
-for i, article in enumerate(news):
+for ticker in midcap_df["Ticker"]:
+  ticker = yf.Ticker(ticker)
+  news = ticker.news
 
-  #get article info
-  #print(article)
-  content = article.get('content')
-  title = content.get('title')
-  summary = content.get('summary')
-  url = content.get('previewUrl') or 'No URL available'
-  #print(url)
-  #print(content.get('url') or 'No URL available')
+  print(f"\n----Finding news for {ticker} ----")
 
-  #skip for irrelevant articles
-  if keyword not in summary.lower():
-    continue
+  #run all pulled articles through finBERT analysis
+  for i, article in enumerate(news):
 
-  #pull finBERT sentiment from article summary
-  analysis = pipe(summary)[0]
-      #later integrate to fully utilize the historical 10k data
-      #parse large docs into small summaries
+    #get article info
+    content = article.get('content')
+    title = content.get('title')
+    summary = content.get('summary')
+    url = content.get('previewUrl') or 'No URL available'
 
-  #gather total sentiment
-  if analysis['label'] == 'positive':
-    total_score += analysis['score']
-  elif analysis['label'] == 'negative':
-    total_score -= analysis['score']
+    #skip for irrelevant articles
+    #if keyword not in summary.lower():
+      #continue
 
-  total_articles += 1
+    #pull finBERT sentiment from article summary
+    analysis = pipe(summary)[0]
 
+    #gather total sentiment
+    if analysis['label'] == 'positive':
+      total_score += analysis['score']
+    elif analysis['label'] == 'negative':
+      total_score -= analysis['score']
 
-  print(f"Title: {title}")
-  print(f"Summary: {summary}")
-  print(url)
-  print(f"Analysis: {analysis}")
-  print("-" * 40)
+    total_articles += 1
 
 
-if total_articles != 0:
-  final_score = total_score / total_articles
-  print(f"Final Score: {final_score}")
-else:
-  print("No articles found.")
+    # print(f"Title: {title}")
+    # print(f"Summary: {summary}")
+    # print(f"Analysis: {analysis}")
+    # print("-" * 40)
+
+
+  if total_articles != 0:
+    final_score = total_score / total_articles
+    print(f"Final Score: {final_score}")
+  else:
+    print("No articles found.")
